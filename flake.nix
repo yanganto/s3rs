7aa8d31fef90f79d2c05e57bdb127f11fdeea284
Antonio Yang <yanganto@gmail.com>
2022-06-13 22:57:33 +0800

    Use costomized dr

--- flake.nix
+++ flake.nix
@@ -4,8 +4,9 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.dependency-refresh.url = "github:yanganto/dependency-refresh";
 
-  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+  outputs = { self, rust-overlay, nixpkgs, flake-utils, dependency-refresh }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -13,8 +14,8 @@
           inherit system overlays;
         };
         rust = pkgs.rust-bin.stable.latest.default;
+        dr = dependency-refresh.defaultPackage.${system};
         updateDependencyScript = pkgs.writeShellScriptBin "update-dependency" ''
-          cargo install dependency-refresh
           dr ./Cargo.toml
           if [ -f "Cargo.toml.old" ]; then
             rm Cargo.toml.old
@@ -33,6 +34,7 @@
             openssl
             pkg-config
             rust
+            dr
             publishScript
             updateDependencyScript
           ];
