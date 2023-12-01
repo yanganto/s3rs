cfe9645ccc420df39927347053026306d8b6b5b0
Antonio Yang <yanganto@gmail.com>
2023-12-01 09:48:40 +0800

    update lock

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         packages.${system}.s3rs = pkgs.rustPlatform.buildRustPackage {
           name = "s3rs";
           src = self;
-          cargoSha256 = "sha256-DRVgD4xhzJcDZ1aEx9Ks76w1Cuqmu2gTNGq42/+Ydxc=";
+          cargoSha256 = "sha256-CdfhRUKg+SkgjQViLbMKjfrhczYMHCI6dbsj7S5tdFs=";
           buildInputs = [ openssl ];
           nativeBuildInputs = [ pkg-config ];
         };
